51472c7a1aa13656fde560adbf0b5da0ac52cf8e
Anand Sharma <anasharm@cisco.com>
2022-11-03 12:31:19 -0500

    Fixed the ID hardcoding

--- 2_ldapsearch.py
+++ 2_ldapsearch.py
@@ -3,10 +3,12 @@ import subprocess
 import sys
 from Crypto.Cipher import AES
 
-file_in = open("data/cec.bin", "rb")
-nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+cec_id = '<your-id>@cisco.com'  # CHANGEME
 
-# let's assume that the key is somehow available again
+file_in = open("data/cec.bin", "rb")
+nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
+
+# read the key used to create the encryption
 with open("data/cec.key", mode="rb") as key_file:
     key = key_file.read()
 cipher = AES.new(key, AES.MODE_EAX, nonce)
@@ -14,13 +16,17 @@ data = cipher.decrypt_and_verify(ciphertext, tag)
 pwd = data.decode()
 
 with open("data/all.csv", "r") as a_file:
-  for line in a_file:
-    user = line.strip()
-    command = 'ldapsearch -LLL -b "OU=Employees,OU=Cisco Users, DC=cisco, DC=com" -D \'anasharm@cisco.com\' -w \'' + pwd + '\' \'(description=' + user + ')\' mail employeeType'
-    ldap = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, bufsize=1, universal_newlines=True)
-    for line in ldap.stdout:
-        sys.stdout.write(line)
-    ldap.wait()
-    exit_code = ldap.returncode
-    if exit_code != 0:
-        raise RuntimeError(f"Shell command failed with exit code {exit_code}. Command: `{command}`")
+    for line in a_file:
+        user = line.strip()
+        command = 'ldapsearch -LLL -b "OU=Employees,OU=Cisco Users, DC=cisco, DC=com" -D \'' + \
+            cec_id + '\' -w \'' + pwd + \
+            '\' \'(description=' + user + ')\' mail employeeType'
+        ldap = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT, shell=True, bufsize=1, universal_newlines=True)
+        for line in ldap.stdout:
+            sys.stdout.write(line)
+        ldap.wait()
+        exit_code = ldap.returncode
+        if exit_code != 0:
+            raise RuntimeError(
+                f"Shell command failed with exit code {exit_code}. Command: `{command}`")
--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 ## Steps
 
 - Make sure you create a file called `data/.cec` with your CEC password in it
+- Open `2_ldapsearch.py` and make sure you enter your CEC Email ID (line 6. Search for `# CHANGEME`)
 - Run `python3 1_cec-encrypt.py`: This will encrypt the password and the key used to encrypt it
 - Run `python3 2_ldapsearch.py`: This will use the encoded password and key from previous step to run `ldapsearch` on each line of the `all.csv` file
 
